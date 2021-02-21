fdaeaf4dd382b111cdba759630a46fbb1b92e6f0
drkwdck <reshetnikov.e.a@mail.ru>
2021-02-21 17:47:32 +0300

    добавлены библиотеки https://dev.to/jakebloom/creating-static-and-dynamic-libraries-for-your-c-programs-3037

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,18 @@
 #include "revert_string.h"
 
+
 void RevertString(char *str)
 {
-	// your code here
-}
+    char  c;
+    char* s0 = str - 1;
+    char* s1 = str;
+    while (*s1) ++s1;
 
+
+    while (s1-- > ++s0)
+    {
+        c = *s0;
+        *s0 = *s1;
+        *s1 =  c;
+    }
+}
